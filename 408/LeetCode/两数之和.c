d2bb8f765e40612c7abff90d4ae8e4bce89307f4
lionsom <lionsom_lin@qq.com>
2021-12-24 20:35:59 +0800

    flutterFramework更新

---
+++ 408/LeetCode/两数之和.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+    int count = sizeof(&nums) / sizeof(nums[0]);
+
+    printf("%d", count);
+
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+
+    return NULL;
+}
+
+int main() {
+
+    twoSum(NULL, 10, 1, NULL);
+
+    int AA[10] = {1,2,3,4,5,6,7,8,9,0};
+
+    printf("hahah");
+
+    return 0;
+}
